aa506364d53d07cb9eb9b3cbf1cc47b828c15f36
James Dean Mathias <dean.mathias@bridgertech.com>
2016-06-26 15:48:54 -0600

    Stubbed in the 2D frustum component.

---
+++ JavaScript/QuadTreeFrustum/scripts/Components/Frustum.js
@@ -0,0 +1,14 @@
+/* global Demo */
+
+//------------------------------------------------------------------
+//
+// Defines a 2D viewing frustum.
+//
+//------------------------------------------------------------------
+Demo.components.Frustum = function(spec) {
+	'use strict';
+	var that = {};
+
+
+	return that;
+};
