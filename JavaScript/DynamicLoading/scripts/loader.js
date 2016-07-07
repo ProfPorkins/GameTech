f28acefbb2e90476158e547dff35e72f38b791c4
James Dean Mathias <dean.mathias@bridgertech.com>
2016-07-07 17:51:03 -0600

    Revised the loader to use a custom dependency technique for defining when to load scripts.  I'm purposely not using more of Require.js features (for my own reasons).

--- JavaScript/DynamicLoading/scripts/loader.js
+++ JavaScript/DynamicLoading/scripts/loader.js
@@ -7,36 +7,85 @@ Demo = {
 };
 
 
+//------------------------------------------------------------------
+//
+// Purpose of this code is to bootstrap (maybe I should use that as the name)
+// the rest of the application.  Only this file is specified in the index.html
+// file, then the code in this file gets all the other code and assets
+// loaded.
+//
+//------------------------------------------------------------------
 Demo.loader = (function() {
 	'use strict';
+	var scriptOrder = [
+		{
+			scripts: ['Input/Keyboard'],
+			message: 'Inputs loaded',
+			onComplete: null
+		},
+		{
+			scripts: ['Components/Text'],
+			message: 'Components loaded',
+			onComplete: null
+		},
+		{
+			scripts: ['Rendering/core'],
+			message: 'Rendering core loaded',
+			onComplete: null
+		},
+		{
+			scripts: ['Rendering/Text'],
+			message: 'Rendering loaded',
+			onComplete: null
+		},
+		{
+			scripts: ['model'],
+			message: 'Model loaded',
+			onComplete: null
+		},
+		{
+			scripts: ['main'],
+			message: 'Main loaded',
+			onComplete: null
+		}
+	];
 
-	function loadScripts(sources, onComplete, message) {
-		require(sources, function() {
-			console.log(message);
+	//------------------------------------------------------------------
+	//
+	// Helper function used to load scripts in the order specified by the
+	// 'scripts' parameter.  'scripts' expects an array of objects with
+	// the following format...
+	//	{
+	//		scripts: [script1, script2, ...],
+	//		message: 'Console message displayed after loading is complete',
+	//		onComplete: function to call when loading is complete, may be null
+	//	}
+	//
+	//------------------------------------------------------------------
+	function loadScripts(scripts, onComplete) {
+		var entry = 0;
+		//
+		// When we run out of things to load, that is when we call onComplete.
+		if (scripts.length > 0) {
+			entry = scripts[0];
+			require(entry.scripts, function() {
+				console.log(entry.message);
+				if (entry.onComplete) {
+					entry.onComplete();
+				}
+				scripts.splice(0, 1);
+				loadScripts(scripts, onComplete);
+			});
+		} else {
 			onComplete();
-		});
+		}
 	}
 
-	function inputsComplete() {
-		loadScripts(['Components/Text'], componentsComplete, 'Components loaded');
-	}
-
-	function componentsComplete() {
-		loadScripts(['Rendering/core'], coreRenderingComplete, 'Rendering core loaded');
-	}
-
-	function coreRenderingComplete() {
-		loadScripts(['Rendering/Text'], renderingComplete, 'Rendering loaded');
-	}
-
-	function renderingComplete() {
-		loadScripts(['model'], modelComplete, 'Model loaded');
-	}
-
-	function modelComplete() {
-		loadScripts(['main'], mainComplete, 'Main loaded');
-	}
-
+	//------------------------------------------------------------------
+	//
+	// Called when all the scripts are loaded, it kicks off the demo app.
+	//
+	//------------------------------------------------------------------
 	function mainComplete() {
 		console.log('it is all loaded up');
 		Demo.main.initialize();
@@ -45,5 +94,5 @@ Demo.loader = (function() {
 	//
 	// Start with the input scripts, the cascade from there
 	console.log('Starting to dynamically load project scripts');
-	loadScripts(['Input/Keyboard'], inputsComplete, 'Inputs loaded');
+	loadScripts(scriptOrder, mainComplete);
 }());
