2b75d90998e9b8adc329791fa00f2620128887e0
James Dean Mathias <dean.mathias@bridgertech.com>
2016-07-24 19:45:41 -0600

    Initial cut at showing the use of custom or embedded fonts.

---
+++ JavaScript/Custom Fonts/README.md
@@ -0,0 +1,8 @@
+# Custom Font Sample
+The sample must be run as a server: `node server.js`
+With the server running, connect to: `http://localhost:3000`
+
+## Content Acknowledgements
+
+* Use of *kenvector_future.ttf* and *kenvector_future_thin.ttf* under Creative Commons License
+  * Source: http://www.kenney.nl
---
+++ JavaScript/Custom Fonts/scripts/model.js
@@ -0,0 +1,105 @@
+/* global Demo */
+
+// ------------------------------------------------------------------
+//
+// This namespace holds the custom font demo model.
+//
+// ------------------------------------------------------------------
+Demo.model = (function(components) {
+	'use strict';
+
+	var textSmall = components.Text({
+			text: 'Small Text',
+			font: '12px font-1',
+			fill: 'rgba(255, 0, 0, 1)',
+			position: { x: 0.0, y: 0.00 }
+		}),
+		textMedium = components.Text({
+			text: 'Medium Text',
+			font: '20px font-1',
+			fill: 'rgba(0, 255, 0, 1)',
+			position: { x: 0.0, y: 0.00 }
+		}),
+		textLarge = components.Text({
+			text: 'Large Text',
+			font: '36px font-1',
+			fill: 'rgba(100, 100, 255, 1)',
+			position: { x: 0.0, y: 0.00 }
+		}),
+		that = {};
+
+	// ------------------------------------------------------------------
+	//
+	// When a resize event occurs, remeasure where things should go.
+	//
+	// ------------------------------------------------------------------
+	function notifyResize() {
+		var totalHeight = 0;
+
+		//
+		// Place all the text items in the center (horizontally) of the screen.
+		textSmall.height = Demo.renderer.core.measureTextHeight(textSmall);
+		textMedium.height = Demo.renderer.core.measureTextHeight(textMedium);
+		textLarge.height = Demo.renderer.core.measureTextHeight(textLarge);
+
+		textSmall.width = Demo.renderer.core.measureTextWidth(textSmall);
+		textMedium.width = Demo.renderer.core.measureTextWidth(textMedium);
+		textLarge.width = Demo.renderer.core.measureTextWidth(textLarge);
+
+		//
+		// Figure out where to start vertically so we center all of the text vertically.
+		totalHeight = textSmall.height + textMedium.height + textLarge.height;
+
+		textSmall.position.x = (0.5 - textSmall.width / 2.0);
+		textSmall.position.y = 0.5 - totalHeight / 2.0 + textSmall.height;
+
+		textMedium.position.x = (0.5 - textMedium.width / 2.0);
+		textMedium.position.y = textSmall.position.y + textSmall.height;
+
+		textLarge.position.x = (0.5 - textLarge.width / 2.0);
+		textLarge.position.y = textMedium.position.y + textMedium.height;
+	}
+
+	// ------------------------------------------------------------------
+	//
+	// This function initializes the input demo model.  Only thing it
+	// does right now is to register the resize event with the renderer.
+	//
+	// ------------------------------------------------------------------
+	that.initialize = function() {
+
+		Demo.renderer.core.notifyResize(notifyResize);
+		notifyResize(true);
+	};
+
+	// ------------------------------------------------------------------
+	//
+	// This function is used to update the state of the demo model.
+	//
+	// ------------------------------------------------------------------
+	that.update = function() {
+		//
+		// We don't have anything to do!
+	};
+
+	// ------------------------------------------------------------------
+	//
+	// This function renders the demo model.
+	//
+	// ------------------------------------------------------------------
+	that.render = function(renderer) {
+
+		//
+		// Render the incredibly interesting text objects
+		renderer.Text.render(textSmall);
+		renderer.Text.render(textMedium);
+		renderer.Text.render(textLarge);
+
+		//
+		// Draw a border around the unit world.
+		renderer.core.drawRectangle('rgba(255, 255, 255, 1)', 0, 0, 1, 1);
+	};
+
+	return that;
+
+}(Demo.components));
