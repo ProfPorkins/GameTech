d05c2d568be485b587945ae88df7576bf92eedfd
Dean Mathias <dean.mathias@bridgertech.com>
2019-12-28 09:57:24 -0700

    Updated the Responsive Canvas demo to the latest and greatest code style and related revisions.

--- JavaScript/ResponsiveCanvas/server.js
+++ JavaScript/ResponsiveCanvas/server.js
@@ -1,16 +1,13 @@
-/* eslint-env node */
-/* eslint no-mixed-requires: ["error", { "grouping": false }] */
-/* eslint one-var: 0 */
 'use strict';
 
-var http = require('http'),
-	path = require('path'),
-	fs = require('fs');
+let http = require('http');
+let path = require('path');
+let fs = require('fs');
 
-var mimeTypes = {
-	'.js' : 'text/javascript',
-	'.html' : 'text/html',
-	'.css' : 'text/css'
+let mimeTypes = {
+    '.js' : 'text/javascript',
+    '.html' : 'text/html',
+    '.css' : 'text/css'
 };
 
 //------------------------------------------------------------------
@@ -20,32 +17,32 @@ var mimeTypes = {
 //
 //------------------------------------------------------------------
 function handleRequest(request, response) {
-	var lookup = (request.url === '/') ? '/index.html' : decodeURI(request.url),
-		file = lookup.substring(1, lookup.length);
+    let lookup = (request.url === '/') ? '/index.html' : decodeURI(request.url);
+    let file = lookup.substring(1, lookup.length);
 
-	console.log('request: ' + request.url);
-	fs.exists(file, function(exists) {
-		if (exists) {
-			console.log('Trying to send: ' + lookup);
-			fs.readFile(file, function(err, data) {
-				var headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
+    console.log('request: ' + request.url);
+    fs.exists(file, function(exists) {
+        if (exists) {
+            console.log('Trying to send: ' + lookup);
+            fs.readFile(file, function(err, data) {
+                let headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
 
-				if (err) {
-					response.writeHead(500);
-					response.end('Server Error!');
-				} else {
-					response.writeHead(200, headers);
-					response.end(data);
-				}
-			});
-		} else {
-			console.log('Failed to find/send: ' + lookup);
-			response.writeHead(404);
-			response.end();
-		}
-	});
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Server Error!');
+                } else {
+                    response.writeHead(200, headers);
+                    response.end(data);
+                }
+            });
+        } else {
+            console.log('Failed to find/send: ' + lookup);
+            response.writeHead(404);
+            response.end();
+        }
+    });
 }
 
 http.createServer(handleRequest).listen(3000, function() {
-	console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
