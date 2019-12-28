1070c2a1bb732da9fcf619456583facb8d409cf1
Dean Mathias <dean.mathias@bridgertech.com>
2019-12-28 10:05:20 -0700

    Updated the Custom Fonts demo to the latest and greatest code style and related revisions.

--- JavaScript/Custom Fonts/server.js
+++ JavaScript/Custom Fonts/server.js
@@ -1,21 +1,18 @@
-/* eslint-env node */
-/* eslint no-mixed-requires: ["error", { "grouping": false }] */
-/* eslint one-var: 0 */
 'use strict';
 
-var http = require('http'),
-	path = require('path'),
-	fs = require('fs');
+let http = require('http');
+let path = require('path');
+let fs = require('fs');
 
 var mimeTypes = {
-	'.js' : 'text/javascript',
-	'.html' : 'text/html',
-	'.css' : 'text/css',
-	'.png' : 'image/png',
-	'.jpg' : 'image/jpeg',
-	'.mp3' : 'audio/mpeg3',
-	'.ttf' : 'application/x-font-ttf',
-	'.eot' : 'application/vnd.ms-fontobject'
+    '.js' : 'text/javascript',
+    '.html' : 'text/html',
+    '.css' : 'text/css',
+    '.png' : 'image/png',
+    '.jpg' : 'image/jpeg',
+    '.mp3' : 'audio/mpeg3',
+    '.ttf' : 'application/x-font-ttf',
+    '.eot' : 'application/vnd.ms-fontobject'
 };
 
 //------------------------------------------------------------------
@@ -25,32 +22,32 @@ var mimeTypes = {
 //
 //------------------------------------------------------------------
 function handleRequest(request, response) {
-	var lookup = (request.url === '/') ? '/index.html' : decodeURI(request.url),
-		file = lookup.substring(1, lookup.length);
+    let lookup = (request.url === '/') ? '/index.html' : decodeURI(request.url);
+    let file = lookup.substring(1, lookup.length);
 
-	console.log('request: ' + request.url);
-	fs.exists(file, function(exists) {
-		if (exists) {
-			console.log('Trying to send: ' + lookup);
-			fs.readFile(file, function(err, data) {
-				var headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
+    console.log('request: ' + request.url);
+    fs.exists(file, function(exists) {
+        if (exists) {
+            console.log('Trying to send: ' + lookup);
+            fs.readFile(file, function(err, data) {
+                let headers = { 'Content-type': mimeTypes[path.extname(lookup)] };
 
-				if (err) {
-					response.writeHead(500);
-					response.end('Server Error!');
-				} else {
-					response.writeHead(200, headers);
-					response.end(data);
-				}
-			});
-		} else {
-			console.log('Failed to find/send: ' + lookup);
-			response.writeHead(404);
-			response.end();
-		}
-	});
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Server Error!');
+                } else {
+                    response.writeHead(200, headers);
+                    response.end(data);
+                }
+            });
+        } else {
+            console.log('Failed to find/send: ' + lookup);
+            response.writeHead(404);
+            response.end();
+        }
+    });
 }
 
 http.createServer(handleRequest).listen(3000, function() {
-	console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 3000');
 });
