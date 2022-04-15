cb60f472eb1491c89577d8c418762c82b333e826
Dean Mathias <dean.mathias@hotmail.com>
2022-04-15 12:39:18 -0600

    Another fix to the loadAsset function to correctly call onSuccess when an Audio file is loaded

--- JavaScript/Multiplayer/Step 5 - Weapons/scripts/client/loader.js
+++ JavaScript/Multiplayer/Step 5 - Weapons/scripts/client/loader.js
@@ -149,36 +149,48 @@ Demo.loader = (function() {
     //------------------------------------------------------------------
     function loadAsset(source, onSuccess, onError) {
         let xhr = new XMLHttpRequest();
-        let asset = null;
         let fileExtension = source.substr(source.lastIndexOf('.') + 1);    // Source: http://stackoverflow.com/questions/680929/how-to-extract-extension-from-filename-string-in-javascript
 
         if (fileExtension) {
             xhr.open('GET', source, true);
-            xhr.responseType = 'blob';
+            xhr.responseType = (fileExtension === 'txt') ? 'text' : 'blob';
 
             xhr.onload = function() {
+                let asset = null;
                 if (xhr.status === 200) {
                     if (fileExtension === 'png' || fileExtension === 'jpg') {
                         asset = new Image();
                     } else if (fileExtension === 'mp3') {
                         asset = new Audio();
-                    } else {
+                    } else if (fileExtension === 'txt') {
+                        if (onSuccess) { onSuccess(xhr.responseText); }
+                    }
+                    else {
                         if (onError) { onError('Unknown file extension: ' + fileExtension); }
                     }
-                    asset.onload = function() {
-                        window.URL.revokeObjectURL(asset.src);
-                        if (onSuccess) { onSuccess(asset); }
-                    };
-                    asset.src = window.URL.createObjectURL(xhr.response);
+                    if (xhr.responseType === 'blob') {
+                        if (fileExtension === 'mp3') {
+                            asset.oncanplaythrough = function() {
+                                window.URL.revokeObjectURL(asset.src);
+                                if (onSuccess) { onSuccess(asset); }
+                            };
+                        }
+                        else {  // not terrific assumption that it has an 'onload' event, but that is what we are doing
+                            asset.onload = function() {
+                                window.URL.revokeObjectURL(asset.src);
+                                if (onSuccess) { onSuccess(asset); }
+                            };
+                        }
+                        asset.src = window.URL.createObjectURL(xhr.response);
+                    }
                 } else {
                     if (onError) { onError('Failed to retrieve: ' + source); }
                 }
             };
+            xhr.send();
         } else {
             if (onError) { onError('Unknown file extension: ' + fileExtension); }
         }
-
-        xhr.send();
     }
 
     //------------------------------------------------------------------
